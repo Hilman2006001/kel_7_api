0823593f582cc123c7ebc9853ab1b62234dd4d7d
Hilman <2006001@itg.ac.id>
2023-06-14 19:05:43 +0700

    first commit

---
+++ lib/utils/endpoint.dart
@@ -0,0 +1,8 @@
+class EndPoint {
+  static const _baseUrl = "https://jsonplaceholder.typicode.com";
+  static const post = "$_baseUrl/posts";
+  static const comment = "$_baseUrl/comments";
+  static const photo = "$_baseUrl/photos";
+  static const todo = "$_baseUrl/todos";
+  static const user = "$_baseUrl/users";
+}
